2ec6a22c586d5299a79f9b2abfb81dfac8d63dc5
appyit3 <arpit.excel@gmail.com>
2020-10-25 12:23:19 +0530

    nodejs app added

---
+++ DailyBudget/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+//var debug = require('debug')('app');
+var morgan = require('morgan');
+
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function(req, res){
+    console.debug('response');
+    res.send('hello yoll');
+})
+
+app.listen(3000, function(){
+    console.debug('listen on port 3000');
+});
